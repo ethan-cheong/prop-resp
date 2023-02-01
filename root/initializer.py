0b87ca05cb6b98b95e74a72e6981daebad51b977
ethan-cheong <ethancheong@hotmail.sg>
2023-02-01 13:13:59 +0000

    Added function for initializing discrete utilities

--- root/initializer.py
+++ root/initializer.py
@@ -45,6 +45,36 @@ class Initializer:
         bids = (util.T/(1.01*util.sum(axis=1))).T
         # TODO: Deal with the rounding to 0 error
         return [budget, bids, util]
+
+    def initialize_linear_utilities_discrete(self, high=2):
+        """
+        Initializes util weights having value 0/1 with 50% probability each. Then normalize utilities to 1.
+
+        Params
+        ------
+        high : int
+            Upper bound (exclusive) for integer utility weights. Default initializes utility weights of either 0 or 1.
+        
+        """
+        util = np.random.randint(0, high, size=(self.n_buyers, self.n_goods))
+        util_row_sums = util.sum(axis=1)
+        for i in range(len(util_row_sums)):
+            if util_row_sums[i]==0:
+                index = np.random.randint(0, len(util[i]))
+                util[i, index] = 1
+            util_row_sums[i] = 1
+        util = util / util_row_sums[:, np.newaxis]
+        
+        # budgets sum to 1 - again in line with zhang's convergence proof
+
+        budget = np.ones(self.n_buyers)
+
+        # bids - initialise start bid as the amount of utility they get from the good?
+        # bids - rescale utilities to sum to budget per good.
+        # multiply by 1.01 for easy fix for loss of precision for floats
+        bids = (util.T/(1.01*util.sum(axis=1))).T
+        # TODO: Deal with the rounding to 0 error
+        return [budget, bids, util]
         
     # TODO: more complicated utility initializations!
 
