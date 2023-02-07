fcd5e2f1e61c855b6af5e893c021f4428c574e3c
ethan-cheong <ethancheong@hotmail.sg>
2023-02-07 17:12:57 +0000

    Modified tests for initialiser

--- tests/initializer_test.py
+++ tests/initializer_test.py
@@ -1,13 +1,36 @@
 import unittest
 from root.initializer import Initializer
-from root.market import Market
+from root.market import PropRespLinearMarket
+from root.simulation import Simulation
 
-class InitializerBasicTest:
+class InitializerBasicTest(unittest.TestCase):
     def test(self):
-        initializer = Initializer(100, 100)
+        initializer = Initializer(50, 1000, 1)
         params = initializer.initialize_linear_utilities_basic()
-        print(params)
-        market = Market(params[0], params[1], params[2])
-        for i in range(10):
+        #print(params)
+        print(params[0].shape) # budget
+        print(params[1].shape) # bids
+        print(params[2].shape) # utils
+
+        market = PropRespLinearMarket(params[0], params[1], params[2])
+        # print(params[0])
+        # print(params[1])
+        # print(np.sum(params[1], axis=1))
+
+        for i in range(1000):
             print(market.get_qty())
-            market.prop_resp_update()
+            market.update()
+
+
+class InitializerDiscreteTest(unittest.TestCase):
+    def test(self):
+        initializer = Initializer(5, 1000, 1)
+        params = initializer.initialize_linear_utilities_discrete()
+        
+        market = PropRespLinearMarket(params[0], params[1], params[2])
+        # print(params[0])
+        # print(params[1])
+        # print(np.sum(params[1], axis=1))
+
+        simulation = Simulation(market)
+        simulation.run(20)
